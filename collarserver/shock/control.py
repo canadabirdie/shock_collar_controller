ecd7042e904a8e390c187c0690ccb6f095a47cf5
canadabirdie <acanadianbird@gmail.com>
2022-03-13 11:56:48 -0400

    Possibly working version?

--- collarserver/shock/control.py
+++ collarserver/shock/control.py
@@ -3,18 +3,23 @@
 # Some module managing nonsense
 from pathlib import Path
 import sys
+
+from django.http import HttpResponse
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 
-from mechanical import presser
 from time import sleep
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from rest_framework.parsers import JSONParser
 
 from .forms import ShockForm
 from .shock import activate
-from .models import Function
+from .models import Function, string_to_func
+
+from mechanical import presser
 
 locked = False
 
@@ -39,3 +44,20 @@ def controller(request):
     
     context = {'form': form, 'on': presser.get_switch_state()}
     return render(request, 'collarserver/controller.html', context)
+
+def api_shock(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(username = data["username"], password = data["password"])
+        if user is None:
+            response = HttpResponse("")
+            response.status_code = 400
+            return response
+        else:
+            mode = string_to_func[data["function"]]
+            global locked
+            while locked:
+                sleep(1)
+            locked = True
+            activate(mode, data['duration'], data['power'])
+            locked = False
--- collarserver/shock/models.py
+++ collarserver/shock/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from enum import IntEnum
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 MINPOWER = 0
 MAXPOWER = 99
@@ -8,3 +9,8 @@ class Function(IntEnum):
     SHOCK = 1
     VIBRATE = 2
     SOUND = 3
+
+string_to_func = {"shock": Function.SHOCK, "vibrate": Function.VIBRATE, "sound": Function.SOUND}
+
+int_to_func = {1: Function.SHOCK, 2: Function.VIBRATE, 3: Function.SOUND}
+func_to_int = {Function.SHOCK: 1, Function.VIBRATE: 2, Function.SOUND: 3}
